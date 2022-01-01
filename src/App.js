cfff8200cd1ac064cea7e7af4bd7cf17d01cfc81
VenomRishi <rishikesh.mhatre@instarem.com>
2022-01-01 16:59:46 +0530

    modified: expenses.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-import ExpenseItems from './components/ExpenseItems';
+import Expenses from './components/Expenses';
 
 function App() {
-  
+  const expenses = [
+    { id: 1, title: 'Banana', amount: 20, date: new Date(2022, 0, 1) },
+    { id: 2, title: 'Eggs', amount: 70, date: new Date(2022, 0, 1) },
+    { id: 3, title: 'Book', amount: 140, date: new Date(2022, 0, 1) },
+    { id: 4, title: 'Pen', amount: 10, date: new Date(2021, 11, 1) },
+]
   return (
     <div className="App">
       <h2>Let's get started</h2>
-      <ExpenseItems/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
