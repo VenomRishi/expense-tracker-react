d16201a27541519a1098e049c39e3dc4bd2d0f7c
VenomRishi <rishikesh.mhatre@instarem.com>
2022-01-02 12:10:26 +0530

    problem statement 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  { id: 1, title: 'Banana', amount: 20, date: new Date(2022, 0, 1) },
+  { id: 2, title: 'Eggs', amount: 70, date: new Date(2022, 0, 1) },
+  { id: 3, title: 'Book', amount: 140, date: new Date(2022, 0, 1) },
+  { id: 4, title: 'Pen', amount: 10, date: new Date(2021, 11, 1) },
+]
+
 const App = () => {
-  const expenses = [
-    { id: 1, title: 'Banana', amount: 20, date: new Date(2022, 0, 1) },
-    { id: 2, title: 'Eggs', amount: 70, date: new Date(2022, 0, 1) },
-    { id: 3, title: 'Book', amount: 140, date: new Date(2022, 0, 1) },
-    { id: 4, title: 'Pen', amount: 10, date: new Date(2021, 11, 1) },
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpense => [expense, ...prevExpense]));
   }
   return (
     <div className="App">
